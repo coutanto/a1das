7c771f648ed1ce9af4f19f83004a08dd2c08cafc
coutanto <60728642+coutanto@users.noreply.github.com>
2023-12-18 16:26:04 +0100

    Delete configure_var.sh

--- configure_var.sh
+++
@@ -1,8 +0,0 @@
-
-#This shell try to determine the good locations and the suffix for applying f2py
-
-
-python configure_var.py > Makefile.python
-
-f2py --help-link f2py_info | grep sources | sed -e "s/sources/F2PY_SRC/" | sed -e "s/\[//" | sed -e "s/\]//" >> Makefile.python
-f2py --help-link f2py_info | grep include_dirs | sed -e "s/include_dirs/F2PY_INC/" | sed -e "s/\[//" | sed -e "s/\]//" >> Makefile.python
